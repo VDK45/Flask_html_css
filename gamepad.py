80b84104b1f405763823fdde4ccbd600ef520e0f
VDK45 <vdk45blg@gmail.ru>
2021-12-12 00:25:04 +0900

    контролер страница

--- gamepad.py
+++ gamepad.py
@@ -1,8 +1,27 @@
 # from flask import Flask, render_template, url_for, request, flask, session, redirect
+import socket
 from flask import *
+from time import sleep
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'wtf.omg,ggg-vdk45'
+
+ip = 'vdk45.ddns.net'
+
+
+def client_send(mes):
+    global ip
+    mes = mes.encode('utf-8')
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        sock.connect((ip, 4545))
+        sock.send(mes)  # send byte
+    except (TimeoutError, OSError) as err:
+        print("Не удалось установить соединение.")
+        print("Проверьте IP адрес!")
+        sock.close()
+        ip_server()
+    sock.close()
 
 
 @app.route('/')
@@ -43,6 +62,7 @@ def register():
     return render_template('gamepad_home.html', title='Registered')
 
 
+# Страница не найдена
 @app.errorhandler(404)
 def pageNotFound(error):
     return render_template('page404.html', title="Страница не найдена"), 404
@@ -65,6 +85,48 @@ def login():
         session['userLongged'] = request.form['username']
         return redirect(url_for('profile', username=session['userLongged']))
     return render_template('login.html', title='Авторизация')
+
+
+@app.route('/controller', methods=["POST", "GET"])
+def controller():
+
+    if request.method == "POST":
+        if request.form['space'] == 'space':
+            client_send('!jump')
+            flash('Отправлено jump', category='success')
+        elif request.form['shoot'] == 'shoot':
+            client_send('!shoot')
+            flash('Отправлено shoot', category='success')
+        elif request.form['pause'] == 'pause':
+            client_send('!pause')
+            flash('Отправлено pause', category='success')
+        elif request.form['up'] == 'up':
+            client_send('!up')
+            flash('Отправлено up', category='success')
+            sleep(0.3)
+            client_send('#up')
+        elif request.form['down'] == 'down':
+            client_send('!down')
+            flash('Отправлено down', category='success')
+            sleep(0.3)
+            client_send('#down')
+        elif request.form['left'] == 'left':
+            client_send('!left')
+            flash('Отправлено left', category='success')
+            sleep(0.3)
+            client_send('#left')
+        elif request.form['right'] == 'right':
+            client_send('!right')
+            flash('Отправлено right', category='success')
+            sleep(0.3)
+            client_send('#right')
+        else:
+            flash('Ошибка отправки или игра не запушена', category='error')
+
+        print(request.form)
+        # print(type(request.form['agree']))
+
+    return render_template('controller.html', title='Controller')
 
 
 if __name__ == '__main__':
